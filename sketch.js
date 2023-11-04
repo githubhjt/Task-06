15a2486794b587dcae8950bf5128d6a07bb1c9e1
jntk.hong <jthonga2@gmail.com>
2023-11-05 01:46:47 +0900

    particles class를 불러와서 checkedges 불러오기

--- sketch.js
+++ sketch.js
@@ -9,22 +9,20 @@ function setup() {
 
 function draw() {
   background(51, 15);
+
   for (let s of systems) {
     s.add(gravity);
     s.addParticle();
-    s.run(); 
-    s.checkEdges();
-    s.checkEdges1();
-  }
+    s.run();
+  } 
+
   if (mouseIsPressed) {
     let mpos = createVector (mouseX, mouseY);
     let s = new ParticleSystem(mpos);
     systems.push(s);
   }
-}
 
-// function mouseClicked() {
-//   let mpos = createVector(mouseX, mouseY);
-//   let s = new ParticleSystem(mpos);
-//   systems.push(s);
-// }
+  let particles = new Particle(createVector);
+  particles.checkEdges();
+  particles.checkEdges1();
+}
