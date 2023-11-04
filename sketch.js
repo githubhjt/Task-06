5090838303204002f0ebd9613c5d4c277274f5c6
jntk.hong <jthonga2@gmail.com>
2023-11-04 19:49:49 +0900

    마우스 클릭효과 없애기
    기존의 코드는 마우스를 클릭해야 파티클이 나오는 구조였다. 파티클의 형태와 모양을 빠르게 만들며 바로바로 확인하기 위해 캔버스 중앙에서 파티클이 나오고 있는 형태로 바꾸어주겠다.

--- sketch.js
+++ sketch.js
@@ -1,48 +1,21 @@
-/*
-let system;
 let gravity;
-let wind;
+let systems;
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 70));
-
-  gravity = createVector(0, 0.005);
-}
-
-function draw() {
-  background(51);
-  system.addParticle();
-  system.run();
-
-  if (mouseIsPressed) {
-    let wind = createVector(0.01, 0);
-    system.add(wind);
-  }
-  system.add(gravity);
-}
-*/
-
-let gravity;
-let systems = [];
-
-function setup() {
-  createCanvas(720, 400);
+  systems = new ParticleSystem(createVector(width/2, 70));
   gravity = createVector(0, 0.05);
 }
 
 function draw() {
   background(51);
 
-  for (let s of systems) {
-    s.addParticle();
-    s.add(gravity);
-    s.run();
-  }
+    systems.addParticle();
+    systems.run();
 }
 
-function mouseClicked() {
-  let mpos = createVector(mouseX, mouseY);
-  let s = new ParticleSystem(mpos);
-  systems.push(s);
-}
+// function mouseClicked() {
+//   let mpos = createVector(mouseX, mouseY);
+//   let s = new ParticleSystem(mpos);
+//   systems.push(s);
+// }
